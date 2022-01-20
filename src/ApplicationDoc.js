f07da964bdc842b3839477783fcb8a6f2a732501
Dev_Shiv <im.shiv.579@gmail.com>
2022-01-20 20:37:10 +0530

    CreateApp-Day1

---
+++ src/ApplicationDoc.js
@@ -0,0 +1,7 @@
+// Api key  dd89a2f8da461cadcbe650b80b96ceba
+
+// Api request https://api.themoviedb.org/3/movie/550?api_key=dd89a2f8da461cadcbe650b80b96ceba
+
+//Api Read access token
+
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDg5YTJmOGRhNDYxY2FkY2JlNjUwYjgwYjk2Y2ViYSIsInN1YiI6IjYxZTk2MjFmYTkxMTdmMDAxYjEyMTQyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kIeC2XAmqRyPYE9D5Qw5R2MG04CNlyY9WcoTOhfAIXg
